8b7cb4c41005a52aafdae66590c12e1f5a7654aa
Rich Gerdes <rich@richgerdes.com>
2016-12-03 19:19:19 -0500

    RoyGoldman Part 1.1

---
+++ RoyGoldman/1/Part1.py
@@ -0,0 +1,42 @@
+
+
+input_file = "part1.txt"
+f = open(input_file, 'r')
+data = f.read()
+
+directions = [x.strip() for x in data.split(',')]
+
+x = 0
+y = 0
+dir = 0
+
+for d in directions:
+    turn = d[0]
+    count = int(d[1:])
+    print dir
+    print count
+    print 
+    if turn == "R":
+        dir += 1
+    else:
+        dir -= 1
+    dir = dir % 4
+    if (dir % 2) == 0:
+        if dir > 1:
+            y -= count
+        else:
+            y += count
+        print "y:"
+        print y
+    else:
+        if dir > 1:
+            x -= count
+        else:
+            x += count
+        print "x:"
+        print x
+
+print x
+print y
+
+print abs(x) + abs(y)
