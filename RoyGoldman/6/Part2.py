969b033f99b71724c09aefa262873215e0ba0d9f
Rich Gerdes <rich@richgerdes.com>
2016-12-06 17:31:27 -0500

    RoyGoldman Day 6

---
+++ RoyGoldman/6/Part2.py
@@ -0,0 +1,21 @@
+with open("Part1.txt") as f:
+    lines = f.readlines()
+
+counts = {}
+
+for line in lines:
+    for i in range(len(line)):
+        if not i in counts.keys():
+            counts[i] = {}
+        c = line[i]
+        if not c in counts[i].keys():
+            counts[i][c] = 0
+        counts[i][c] += 1
+final = ""
+
+for key, chars in counts.items():
+    for char, count in sorted(chars.items(), key=lambda (k, v): chars[k]):
+        final += char
+        break
+    
+print final
