8ff9d66be70e7de896b98eefcabd3c9753720758
KingLinkTiger <KingLinkTiger@gmail.com>
2016-12-06 16:36:58 -0500

    KingLinkTiger: Day 6 golf

--- KingLinkTiger/6/6/Day6.py
+++ KingLinkTiger/6/6/Day6.py
@@ -9,8 +9,7 @@ testdata_columns = zip(*lines)
 
 for line in testdata_columns:
     outputCode += collections.Counter(line).most_common()[0][0]
-    least_common = collections.Counter(line).most_common()[-1]
-    outputCode_Part2 += least_common[0]
+    outputCode_Part2 += collections.Counter(line).most_common()[-1][0]
     
 print "Part 1: " + outputCode
 print "Part 2: " + outputCode_Part2
